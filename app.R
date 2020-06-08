2958b7784602f602ef504eed2451dfa4ffd8b27b
samir <samir.arman@gmail.com>
2020-06-07 22:43:35 -0300

    Adding credit for the data in the proper way.

--- app.R
+++ app.R
@@ -39,41 +39,40 @@ brazil <- states %>%
 rm(data)
 
 # Prepare the selectInput entries -----------------
-places <- c(
-  "BRASIL",
-  levels(states$state),
-  levels(as.factor(cities$city))
-)
+places <- c("BRASIL",
+            levels(states$state),
+            levels(as.factor(cities$city)))
 
 # Define UI for application that draws a histogram
-ui <- fluidPage( # Application title
-  titlePanel("Dados Covid-19 Brasil"),
-
+ui <- fluidPage(# Application title
+  titlePanel("Covid-19 no Brasil"),
+  
   # Sidebar with a slider input for number of bins
   sidebarLayout(
     sidebarPanel(
-      selectInput(
-        "place",
-        "Selecione a localidade",
-        places
-      ),
+      helpText("Dados nacionais, estaduais e municipais."),
+      selectInput("place",
+                  "Selecione a localidade",
+                  places),
       width = 3
     ),
-    mainPanel(plotOutput(
-      "total_cases",
-      height = "800"
+    mainPanel(
+      column(12,
+      plotOutput("total_cases",
+                 height = "800"),
+      p("Fonte: Secretarias de Saúde das Unidades Federativas.\n
+        Dados tratados por Álvaro Justen e colaboradores/", a("Brasil.IO", href="https://brasil.io/"))
     ))
-  )
-)
+  ))
 
 
 # Define server logic required to draw the plots
 server <- function(input, output) {
   output$total_cases <- renderPlot({
     req(input$place)
-
+    
     df <- brazil
-
+    
     if (nchar(as.character(input$place)) == 2) {
       df <- states %>%
         filter(state == input$place)
@@ -81,78 +80,90 @@ server <- function(input, output) {
       df <- cities %>%
         filter(city == input$place)
     }
-
+    
     # Disable warnings temporarily
     # because EpiEstim throws a lot of
     # messages and warnings by default.
     options(warn = -1)
-
+    
     rt <- NULL
-
+    
     if (length(df$new_confirmed[df$new_confirmed > 1]) > 1) {
-
       # Default usage of estimate_R generates a message
       # every time it is used, so we silence it.
-      rt <- suppressMessages(
-        estimate_R(
-          as.incidence(df$new_confirmed, df$date),
-          method = "parametric_si",
-          config = make_config(
-            list(
-              mean_si = covid_si_mean,
-              std_si = covid_si_sd
-            )
-          )
-        )
-      )
+      rt <- suppressMessages(estimate_R(
+        as.incidence(df$new_confirmed, df$date),
+        method = "parametric_si",
+        config = make_config(list(
+          mean_si = covid_si_mean,
+          std_si = covid_si_sd
+        ))
+      ))
     }
-
+    
     p1 <- ggplot(df, aes(x = date, y = last_available_confirmed)) +
-      geom_col() +
-      geom_smooth(method = "loess", formula = y ~ x) +
+      geom_col(color = "gray70") +
+      geom_smooth(method = "loess",
+                  formula = y ~ x,
+                  size = 0.7) +
       ylab("Casos totais") +
       xlab("Data") +
-      scale_y_continuous(labels = function(x) format(x, scientific = FALSE))
-
+      scale_y_continuous(
+        labels = function(x)
+          format(x, scientific = FALSE)
+      )
+    
     p2 <- ggplot(df, aes(x = date, y = last_available_deaths)) +
-      geom_col() +
+      geom_col(color = "gray70") +
+      geom_smooth(method = "loess",
+                  formula = y ~ x,
+                  size = 0.7) +
       ylab("Mortos totais") +
       xlab("Data") +
-      scale_y_continuous(labels = function(x) format(x, scientific = FALSE))
-
+      scale_y_continuous(
+        labels = function(x)
+          format(x, scientific = FALSE)
+      )
+    
     p3 <- ggplot(df, aes(x = date, y = new_confirmed)) +
-      geom_col() +
-      geom_smooth(method = "loess", formula = y ~ x) +
+      geom_col(color = "gray70") +
+      geom_smooth(method = "loess",
+                  formula = y ~ x,
+                  size = 0.7) +
       ylab("Novos casos") +
       xlab("Data") +
-      scale_y_continuous(labels = function(x) format(x, scientific = FALSE))
-
-
+      scale_y_continuous(
+        labels = function(x)
+          format(x, scientific = FALSE)
+      )
+    
+    
     p4 <- ggplot(df, aes(x = date, y = new_deaths)) +
-      geom_col() +
-      geom_smooth(method = "loess", formula = y ~ x) +
+      geom_col(color = "gray70") +
+      geom_smooth(method = "loess",
+                  formula = y ~ x,
+                  size = 0.7) +
       ylab("Novas mortes") +
       xlab("Data") +
-      scale_y_continuous(labels = function(x) format(x, scientific = FALSE))
-
-
-
+      scale_y_continuous(
+        labels = function(x)
+          format(x, scientific = FALSE)
+      )
+    
+    
+    
     rt_plot <- ggplot() +
       labs("Not enough data for calculating R(t)")
-
+    
     if (!is.null(rt)) {
       rt_plot <- estimate_R_plots(rt, what = "R")
     }
-
-    grid.arrange(
-      grobs = list(p1, p2, p3, p4, rt_plot),
-      layout_matrix = rbind(
-        c(1, 2),
-        c(3, 4),
-        c(5, 5)
-      )
-    )
-
+    
+    grid.arrange(grobs = list(p1, p2, p3, p4, rt_plot),
+                 layout_matrix = rbind(c(1, 2),
+                                       c(3, 4),
+                                       c(5, 5)))
+    
     # Turn warnings on again
     options(warn = 1)
   })
