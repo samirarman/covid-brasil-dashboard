32e7b4328e2b823e53e1363b1e5a35c7e750409e
samir <samir.arman@gmail.com>
2020-06-10 01:24:25 -0300

    Create unique key to identify city - Fixes #6

--- get_data.R
+++ get_data.R
@@ -1,25 +1,53 @@
 library(dplyr)
+library(readr)
+library(lubridate)
+library(purrr)
 
 
 get_data <- function() {
-  con <- gzcon(url("https://data.brasil.io/dataset/covid19/caso_full.csv.gz"))
+  con <-
+    gzcon(url(
+      "https://data.brasil.io/dataset/covid19/caso_full.csv.gz"
+    ))
   
   txt <- readLines(con)
+  close(con)
   data <- dplyr::tibble(read.csv(textConnection(txt)))
-  data$date <- as.Date(data$date)
+  data$date <- ymd(data$date)
   
   # Some data points show negative new deaths and negative new cases
   data$new_confirmed[which(data$new_confirmed < 0)] <- 0
   data$new_deaths[which(data$new_deaths < 0)] <- 0
   data$city <- as.character(data$city)
   
+  data <- data %>% split(., .$place_type)
+  
+  data$city <- data$city %>%
+    mutate(key = paste(city, state, sep = " - "))
+  
+  data$state <- data$state %>%
+    mutate(key = state)
+  
   # Remove cities with zero confirmed cases
-  data <- data %>%
-    group_by(city) %>% 
-    filter(sum(new_confirmed) > 0) %>%
-    select(date, city, state, place_type, last_available_confirmed, last_available_deaths, new_confirmed, new_deaths)
-
+  data$city <- data$city %>%
+    filter(sum(new_confirmed) > 0)
+  
+  data %>%
+    map(
+      ~ select(
+        .,
+        key,
+        date,
+        city,
+        state,
+        place_type,
+        last_available_confirmed,
+        last_available_deaths,
+        new_confirmed,
+        new_deaths
+      )
+    )
+  
   data
-
+  
 }
-
